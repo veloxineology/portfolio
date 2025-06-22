a4d07011aa09441f9a1be64bec5f856ea4a5172c
Kaushik <veloxineology@gmail.com>
2025-06-22 16:07:59 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Kaushik S',
+  description: 'Created with Cursor',
+  generator: 'kaushikieee.dev',
 }
 
 export default function RootLayout({
