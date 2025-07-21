bd375e852957c5abe91cadf6c04df27eec1eeeca
Kaushik <veloxineology@gmail.com>
2025-07-21 22:49:45 +0530

    feat: add 'back to friends - Sombr' resource with password protection

--- lib/resources-data.tsx
+++ lib/resources-data.tsx
@@ -11,4 +11,9 @@ export const resources: ResourceItem[] = [
   //   fileLink: "https://github.com/yourusername/yourrepo/raw/main/sample.pdf",
   //   password: "1234",
   // },
+  {
+    title: "back to friends - Sombr",
+    fileLink: "https://github.com/veloxineology/portfolio/raw/main/resources/sombr%20-%20back%20to%20friends.mp3",
+    password: "sombr",
+  },
 ]; 
